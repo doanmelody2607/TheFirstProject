3d6e8161d4ceff4cd7e747be134625a6ee8bd515
doanmelody2607 <doanmelody2607@gmail.com>
2020-12-24 17:33:37 +0700

    demo01

---
+++ src/components/InfoSection/Data.jsx
@@ -0,0 +1,111 @@
+import OffRoadImage from "../../images/off_road.svg";
+import SavingImage from "../../images/saving.svg";
+import SecureDataImage from "../../images/secure_data.svg";
+import WalletImage from "../../images/wallet.svg";
+export const homeObjOne = {
+  id: "about",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Premium Bank",
+  headLine: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited without getting charged any fees.",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: OffRoadImage,
+  alt: "Car",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headLine: "Unlimited Transactions with zero fees",
+  description:
+    "We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: SavingImage,
+  alt: "Piggybank",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "services",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Join our Team",
+  headLine: "Creating an account is extremely easy",
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: "Start Now",
+  imgStart: false,
+  img: SecureDataImage,
+  alt: "PapperSensitive",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjFour = {
+  id: "services",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Join our Team",
+  headLine: "Creating an account is extremely easy",
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: "Start Now",
+  imgStart: true,
+  img: WalletImage,
+  alt: "Wallet",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjFive = {
+  id: "services",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Join our Team",
+  headLine: "Creating an account is extremely easy",
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: "Start Now",
+  imgStart: false,
+  img: SecureDataImage,
+  alt: "PapperSensitive",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjSix = {
+  id: "services",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Join our Team",
+  headLine: "Creating an account is extremely easy",
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: "Start Now",
+  imgStart: false,
+  img: SecureDataImage,
+  alt: "PapperSensitive",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
